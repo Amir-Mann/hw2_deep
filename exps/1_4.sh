92fae4c24137149b0d8795cba96177a57c206e63
Amir Mann <amir.mann@campus.technion.ac.il>
2023-05-31 22:56:43 +0300

    exps1.4 and hidden layers test

--- exps/1_4.sh
+++ exps/1_4.sh
@@ -28,7 +28,7 @@
 NUM_NODES=1
 NUM_CORES=2
 NUM_GPUS=1
-JOB_NAME="test_hp"
+JOB_NAME="exp1.1"
 MAIL_USER="amir.mann@campus.technion.ac.il"
 MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
@@ -52,18 +52,16 @@ echo "*** SLURM BATCH JOB '$JOB_NAME' STARTING ***"
 
 # Setup the conda env
 echo "*** Activating environment $CONDA_ENV ***"
-source $CONDA_HOME/etc/fprofile.d/conda.sh
+source $CONDA_HOME/etc/profile.d/conda.sh
 conda activate $CONDA_ENV
 
 # Run python with the args to the script
-python -m hw2.experiments run-exp -s 20773 -H 110 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.0000001 -K 32 -L 2
-python -m hw2.experiments run-exp -s 20773 -H 70 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.0000005 -K 32 -L 2
-python -m hw2.experiments run-exp -s 20773 -H 50 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.000001  -K 32 -L 2
-python -m hw2.experiments run-exp -s 20773 -H 90 70 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.0000001 -K 32 -L 2
-python -m hw2.experiments run-exp -s 20773 -H 90 50 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.0000005 -K 32 -L 2
-python -m hw2.experiments run-exp -s 20773 -H 90 30 -P 1 --epochs 30 --reg 0.00005 --lr 0.0005 -n test_L2_K32_lr0.000001  -K 32 -L 2
-
-
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 3 -n exp1_4_L8_K32 -K 32 -L 8
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 5 -n exp1_4_L16_K32 -K 32 -L 16
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 9 -n exp1_4_L32_K32 -K 32 -L 32
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 2 -n exp1_4_L2_K64-128-256 -K 64 128 256 -L 2
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 4 -n exp1_4_L4_K64-128-256 -K 64 128 256 -L 4
+python -m hw2.experiments run-exp -s 20773 -H 90 -M resnet -P 8 -n exp1_4_L8_K64-128-256 -K 64 128 256 -L 8
 echo "*** SLURM BATCH JOB '$JOB_NAME' DONE ***"
 EOF
 
