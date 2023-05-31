562cffb3d4f55ad5548c4eb3054c6ebaa697c566
Amir Mann <amir.mann@campus.technion.ac.il>
2023-05-31 17:03:48 +0300

    forwarded training back to after implementation

--- hw2/training.py
+++ hw2/training.py
@@ -90,6 +90,9 @@ class Trainer(abc.ABC):
             test_result = self.test_epoch(dl_test, **kw)
             test_acc.append(test_result.accuracy)
             test_loss += [x.detach() for x in test_result.losses]
+            
+            #creating detatched version of the tendors in train loss
+            
             # ========================
 
             # TODO:
@@ -308,7 +311,9 @@ class ClassifierTrainer(Trainer):
 class LayerTrainer(Trainer):
     def __init__(self, model, loss_fn, optimizer):
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
         # ========================
 
     def train_batch(self, batch) -> BatchResult:
@@ -321,7 +326,13 @@ class LayerTrainer(Trainer):
         #  - Calculate number of correct predictions (make sure it's an int,
         #    not a tensor) as num_correct.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        forward_output = self.model.forward(X.view(X.size(0), -1))
+        loss = self.loss_fn.forward(forward_output, y)
+        dl_doutput = self.loss_fn.backward(loss)
+        self.model.backward(dl_doutput)
+        self.optimizer.step()
+        y_pred = torch.argmax(forward_output, dim = 1)
+        num_correct = (y_pred == y).sum().item()
         # ========================
 
         return BatchResult(loss, num_correct)
@@ -331,7 +342,10 @@ class LayerTrainer(Trainer):
 
         # TODO: Evaluate the Layer model on one batch of data.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        forward_output = self.model.forward(X.view(X.size(0), -1))
+        loss = self.loss_fn.forward(forward_output, y)
+        y_pred = torch.argmax(forward_output, dim = 1)
+        num_correct = (y_pred == y).sum().item()
         # ========================
 
         return BatchResult(loss, num_correct)
