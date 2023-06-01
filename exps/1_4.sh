769ced16ffbbe136bb6eb1022fe2f97ad65a55e9
Amir Mann <amir.mann@campus.technion.ac.il>
2023-06-01 11:07:08 +0300

    Changed exps job_name to be the exp

--- exps/1_4.sh
+++ exps/1_4.sh
@@ -28,7 +28,7 @@
 NUM_NODES=1
 NUM_CORES=2
 NUM_GPUS=1
-JOB_NAME="exp1.1"
+JOB_NAME="exp1.4"
 MAIL_USER="amir.mann@campus.technion.ac.il"
 MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
